761cde2e51a6cfb22bfaae6799305d3736fa46f7
Anna Horianina <anna.goryanina88@gmail.com>
2021-02-08 22:57:43 +0300

    added slider

--- src/main/cardProduct/cardMain.js
+++ src/main/cardProduct/cardMain.js
@@ -2,18 +2,13 @@ import templateModal from '../cardProduct/templates/templateModal.hbs'
 // import '../cardProduct/cardProduct.scss'
 import axios from 'axios'
 import '../cardProduct/image/Vector.svg'
+import initSlider from '../cardProduct/slider'
 const markupId = document.querySelector('#root')
 const openModalBtn=document.querySelector('button[data-action="open-modal"]')
 console.log(openModalBtn)
 
-// const closeModalBtn=document.querySelector('button[data-action="close-modal"]')
-
-// const backdropRef =document.querySelector('.js-backdrop')
-
 openModalBtn.addEventListener('click', onOpenModal)
-//  closeModalBtn.addEventListener('click', onCloseModal)
- 
-// backdropRef.addEventListener('click',onBackDropClick)
+
 async function onOpenModal(){
   // const testProduct= 'https://callboard-backend.goit.global/call/find?search=car'
   const testProduct='https://callboard-backend.goit.global/call/find?search=dog'
@@ -24,36 +19,25 @@ async function onOpenModal(){
   const infoBtn = document.querySelector('.infoBtn')
   infoBtn.addEventListener('click', onClickInfo)
   
-  // const modal= document.querySelector('.modal')
   const close=document.querySelector('.close')
   const backdropRef =document.querySelector('.backdrop')
   
   backdropRef.addEventListener('click',windowOnClick)
   close.addEventListener('click', closeOnClick)
-  
-  
-  
-  // backdropRef.remove()
   }
   function windowOnClick(event){
     console.dir(event.target)
     if(event.target.className!=='backdrop'){
       return 
     }
-    // if (event.target === modal) {
-    //   modal.style.display = "none";
-    // }
-    // backdropRef.remove()
+    
     event.target.remove()
   }
   function closeOnClick(event){
     document.querySelector('.backdrop').remove()
     // event.target.remove()
   }
-  // backdropRef.querySelector.remove()
-  // function onCloseModal (){
-  //   document.body.classList.remove('show-modal')
-  // }
+  
   
 
 function onClickInfo (event){
@@ -61,117 +45,4 @@ console.dir(event.target.textContent)
 event.target.textContent = event.target.textContent === 'Информация о продавце'?'Ольга на ОЛХ':'Информация о продавце';
 }
 
-    // console.log(data)
-
-// const markup  = templateModal();
-// const markupId = document.querySelector('#root')
-// markupId.insertAdjacentHTML('beforeend', markup)
-// })
-
-
-// showBtn = document.querySelector('.showBtn-description');
-// openModalBtn.addEventListener('click', ()=>{
-//   document.body.classList.add('show-modal')
-//   const testProduct= 'https://callboard-backend.goit.global/call/find?search=car'
-  
-//   const{data}=axios.get(testProduct).then(({data})=>{
-//     // console.log(data)
-//     markupId.insertAdjacentHTML('beforeend', templateModal(data[0])) 
-//     const infoBtn = document.querySelector('.infoBtn')
-//     infoBtn.addEventListener('click', function(event){
-//       // event.preventDefault();
-//       showBtn.classList.toggle("js-infoBtn-show");
-//       // event.target.textContent = event.target.textContent === 'Информация о продавце'?'Ольга на ОЛХ':'Информация о продавце';
-//       console.dir(event)
-//       // event.target.textContent = 'Jkmuf yf JK{'
-//     })
-//   })
-  // console.log(data)
-  
-
-// })
-
-// closeModalBtn.addEventListener('click', ()=>{
-//   document.body.classList.remove('show-modal')
-// })
-// backdropRef.addEventListener('click',event=>{
-//   // console.log(event)
-// })
-
-
-// console.log(infoBtn)
-// const button = document.getElementById('button');
-// const menu = document.getElementById('menu');
-
-// button.addEventListener('click', () =>{
-//     if(button.dataset.trigger === 'false'){
-//         button.innerText = 'Закрыть';
-//         menu.style.display = 'block';
-//         button.dataset.trigger = true;
-//     }else{
-//         button.innerText = button.dataset.text;
-//         menu.style.display = 'none';
-//         button.dataset.trigger = false;
-//     }
-// });
-
-// const productsUrl = 'https://callboard-backend.goit.global/call?page=1'
-
-// const fetch = async()=>{
-//   const {data} = await axios.get(`${productsUrl}`)
-//   const products = Object.entries(data)
-//   const product = products.map(item=>{
-//     return{
-//       title: item[0], description: item [1], price: item[2], imageUrls: item[3]
-//     }
-//   })
-//   console.log(product)
-// }
-
-
-// const productsUrl = 'https://callboard-backend.goit.global/call?page=1';
-
-// const ssmc= 'https://callboard-backend.goit.global/call/find?search=ssmc'
-// const Data = async ()=> {
-//   const{data}=await axios.get(ssmc)
-//   console.log(data)
-//   templateModal(data)
-// }
-
-// Data()
-// const getData = async () => {
-//   const { data: categoriesData } = await axios.get(productsUrl);
-// //   const { data: rusCategoriesData } = await axios.get(rusCategoriesUrl);
-// console.log(categoriesData);
-// //   console.log(rusCategoriesData);
-// //   const categoriesMap = categoriesData.reduce((acc, item, index) => {
-// //       return {
-// //           ...acc,
-// //           [item]: rusCategoriesData[index]
-// //       }       
-// //   }, {});
-  
-//   const { data: categories } = await axios.get('${productsUrl}');  
-//   console.log(categories);
-//   // console.log(categoriesMap);
-  
-//   const categoriesWithProducts = Object.entries(categories).map(item => {
-//       return {
-//           title: categoriesData[item[0]] ? categoriesData[item[0]] : item[0],
-//           products: item[1]
-          
-//       }
-//   });
-//   return categoriesWithProducts;
-// }
-
-// const render = async () => {
-//   const data = await getData();
-
-//   const markap = productTpl(data);
-//   markupId.insertAdjacentHTML('beforeend', markap);
- 
-//   return data;    
-// };
-
-// render();
+   
---
+++ src/main/cardProduct/slider.js
@@ -0,0 +1,30 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
+function initSlider({
+  selector = '.swiper-container',
+  slidesPerView = 1,
+}) {
+  return new Swiper(selector,
+{
+//   direction: 'vertical',
+  // loop: true,
+
+  // If we need pagination
+  slidesPerView,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+}
+
+
+export default initSlider;
