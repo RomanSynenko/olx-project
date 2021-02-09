56931cef99b781a28295af1ab02f025079f68300
Anna Horianina <anna.goryanina88@gmail.com>
2021-02-10 00:22:22 +0300

    added infoBtn

--- src/main/cardProduct/cardMain.js
+++ src/main/cardProduct/cardMain.js
@@ -9,40 +9,81 @@ console.log(openModalBtn)
 
 openModalBtn.addEventListener('click', onOpenModal)
 
-async function onOpenModal(){
-  // const testProduct= 'https://callboard-backend.goit.global/call/find?search=car'
-  const testProduct='https://callboard-backend.goit.global/call/find?search=dog'
+async function onOpenModal(event){
+  const testProduct= 'https://callboard-backend.goit.global/call/find?search=car'
+  // const searchRef = event.target.dataset.search
+  // const testProduct=`https://callboard-backend.goit.global/call/find?search=${searchRef}`
   const{data}=await axios.get(testProduct)
+  console.log(data)
   
   markupId.insertAdjacentHTML('beforeend', templateModal(data[0]))
   
   const infoBtn = document.querySelector('.infoBtn')
-  infoBtn.addEventListener('click', onClickInfo)
+  infoBtn.addEventListener('click', async (event)=>{
+   const userId = event.target.getAttribute('data-userId')
+      const userRequest = `https://callboard-backend.goit.global/user/${userId}`
+      const{data}= await axios.get(userRequest)
+      console.log(data)
+      // if(event.target.className==='infoBtn'){
+      //   event.target.textContent.classList.add('infoBtn-open')
+    
+      //  `Ольга на ОЛХ c ${registrationDate} ${email}`
+      // }else{
+      //   'Информация о продавце'
+      // }
+      event.target.classList.add('infoBtn-open')
+      event.target.textContent = `Продавец ${data.registrationDate}\n${data.email} `
+      
+  })
   
+
   const close=document.querySelector('.close')
   const backdropRef =document.querySelector('.backdrop')
   
   backdropRef.addEventListener('click',windowOnClick)
   close.addEventListener('click', closeOnClick)
-  }
-  function windowOnClick(event){
-    console.dir(event.target)
-    if(event.target.className!=='backdrop'){
-      return 
-    }
-    
-    event.target.remove()
-  }
-  function closeOnClick(event){
-    document.querySelector('.backdrop').remove()
-    // event.target.remove()
-  }
-  
-  
-
-function onClickInfo (event){
-console.dir(event.target.textContent)
-event.target.textContent = event.target.textContent === 'Информация о продавце'?'Ольга на ОЛХ':'Информация о продавце';
+  // initSlider()
 }
 
-   
+function windowOnClick(event){
+  console.dir(event.target)
+  if(event.target.className!=='backdrop'){
+    return 
+  } 
+  event.target.remove()
+}
+
+function closeOnClick(event){
+  document.querySelector('.backdrop').remove()
+}
+  
+// function onClickInfo (event){
+//   console.dir(event.target.textContent)
+//   event.target.textContent = event.target.textContent === 'Информация о продавце'?'Ольга на ОЛХ':'Информация о продавце';
+// }
+
+
+{/* <div class="imgWrapper">
+
+<div class="image">
+
+  <img class="image_main" src="{{this}}" alt="">
+
+</div>
+</div>
+<div class="imgWrapper">
+
+<div class="image">
+
+  <img class="image_main" src="{{this}}" alt="">
+
+</div>
+</div>
+<div class="imgWrapper">
+
+<div class="image">
+
+  <img class="image_main" src="{{this}}" alt="">
+
+</div>
+</div> */}
