040b567d72c8e5da31841aad4aef800b3a9dcbc1
Alexander <pashuk.sasha18@gmail.com>
2021-02-13 19:19:55 +0200

    initial commit

--- src/main/footer/footer.js
+++ src/main/footer/footer.js
@@ -1,8 +1,35 @@
 import './styles.css';
 import footerOpen from './templates/footer_render.hbs';
+import openStudentsModal from './templates/students-modal.hbs';
 
-const markupId = document.querySelector('#root')
+
+const markupFooterRef = document.querySelector('#root')
+markupFooterRef.insertAdjacentHTML('beforeend', footerOpen())
 
 
-markupId.insertAdjacentHTML('beforeend', footerOpen())
+const studentOpenBtn = document.querySelector('.js-students');
+studentOpenBtn.addEventListener('click', handleOpenStudentsWindow);
+
+function handleOpenStudentsWindow () {
+    markupFooterRef.insertAdjacentHTML('beforeend', openStudentsModal());
+    const modalRef = document.querySelector('.student-modal')
+    modalRef.classList.add('show-modal');
+    window.addEventListener('keydown', handleKeypress)
+
+    const studentCloseBtn = document.querySelector('.student-modal__close-btn')
+studentCloseBtn.addEventListener('click', handleCloseStudentsWindow)
+
+
+
+function handleCloseStudentsWindow (event) {
+    event.preventDefault()
+    modalRef.classList.remove('show-modal')
+    window.removeEventListener('keypress', handleKeypress)
+}
+}
+
+function handleKeypress({ code }){
+    code === 'click'  && handleOpenStudentsWindow()
+    code === 'Escape'  && handleCloseStudentsWindow()
+  }
 
