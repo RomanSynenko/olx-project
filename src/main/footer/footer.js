a8b72ee5a0b3f42e477e13907a71463b95e9a192
Roman Synenko <synenko6@gmail.com>
2021-02-13 20:32:41 +0200

    debagFoto

--- src/main/footer/footer.js
+++ src/main/footer/footer.js
@@ -10,26 +10,26 @@ markupFooterRef.insertAdjacentHTML('beforeend', footerOpen())
 const studentOpenBtn = document.querySelector('.js-students');
 studentOpenBtn.addEventListener('click', handleOpenStudentsWindow);
 
-function handleOpenStudentsWindow () {
+function handleOpenStudentsWindow() {
     markupFooterRef.insertAdjacentHTML('beforeend', openStudentsModal());
     const modalRef = document.querySelector('.student-modal')
     modalRef.classList.add('show-modal');
     window.addEventListener('keydown', handleKeypress)
 
     const studentCloseBtn = document.querySelector('.student-modal__close-btn')
-studentCloseBtn.addEventListener('click', handleCloseStudentsWindow)
+    studentCloseBtn.addEventListener('click', handleCloseStudentsWindow)
 
 
 
-function handleCloseStudentsWindow (event) {
-    event.preventDefault()
-    modalRef.classList.remove('show-modal')
-    window.removeEventListener('keypress', handleKeypress)
+    function handleCloseStudentsWindow(event) {
+        event.preventDefault()
+        modalRef.classList.remove('show-modal')
+        window.removeEventListener('keypress', handleKeypress)
+    }
 }
-}
 
-function handleKeypress({ code }){
-    code === 'click'  && handleOpenStudentsWindow()
-    code === 'Escape'  && handleCloseStudentsWindow()
-  }
+function handleKeypress({ code }) {
+    code === 'click' && handleOpenStudentsWindow()
+    code === 'Escape' && handleCloseStudentsWindow()
+}
 
