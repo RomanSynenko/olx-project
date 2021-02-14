c32a7dfe167e3d720c3cf0a66925af8a20e8aaa0
Roman Synenko <synenko6@gmail.com>
2021-02-14 22:58:39 +0200

    escclose

--- src/main/footer/footer.js
+++ src/main/footer/footer.js
@@ -4,7 +4,7 @@ import openStudentsModal from './templates/students-modal.hbs';
 
 
 const markupFooterRef = document.querySelector('#root')
-markupFooterRef.insertAdjacentHTML('beforeend', footerOpen())
+markupFooterRef.insertAdjacentHTML('beforeend', footerOpen());
 
 
 const studentOpenBtn = document.querySelector('.js-students');
@@ -15,21 +15,24 @@ function handleOpenStudentsWindow() {
     const modalRef = document.querySelector('.student-modal')
     modalRef.classList.add('show-modal');
     window.addEventListener('keydown', handleKeypress)
-
     const studentCloseBtn = document.querySelector('.student-modal__close-btn')
     studentCloseBtn.addEventListener('click', handleCloseStudentsWindow)
-
-
 
     function handleCloseStudentsWindow(event) {
         event.preventDefault()
         modalRef.classList.remove('show-modal')
         window.removeEventListener('keypress', handleKeypress)
     }
+    const onCloseESC = (event) => {
+        if (event.key === 'Escape') {
+            handleCloseStudentsWindow(event);
+            return
+        }
+    };
+    window.addEventListener('keydown', onCloseESC);
 }
 
 function handleKeypress({ code }) {
     code === 'click' && handleOpenStudentsWindow()
-    code === 'Escape' && handleCloseStudentsWindow()
 }
 
